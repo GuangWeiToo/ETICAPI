ef7c1c81fa0c0ba343ca5502acb293f06ef6cd18
GuangWeiToo <jasonmechete@gmail.com>
2024-10-20 17:39:22 -0400

    api

--- ETICSupaAPI/api/index.js
+++ ETICSupaAPI/api/index.js
@@ -144,6 +144,26 @@ app.post("/updateProgress", async (req, res) => {
   await userRequest();
 });
 
+// test Route
+app.get("/test", async (req, res) => {
+  const {key} = req.query;
+  const verificationResult = verifyApplication(key);
+  if (verificationResult) {
+    return res.status(403).send(verificationResult);
+  }
+  const userRequest = async () => {
+    const {data} = "Connected and Works"
+
+    if (error) {
+      return res.status(500).send("Error in API VERCEL");
+    }
+
+    // Serve the result after the SQL request finishes
+    res.status(200).json(data);
+    return data;
+  };
+  await userRequest();
+});
 
 /*
 // Start the server * must be changed later
