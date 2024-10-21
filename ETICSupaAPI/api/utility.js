f04bcb7281a4e08a5bba614de9a26b46cc3a717b
GuangWeiToo <jasonmechete@gmail.com>
2024-10-20 21:56:02 -0400

    api

--- ETICSupaAPI/api/utility.js
+++ ETICSupaAPI/api/utility.js
@@ -2,7 +2,7 @@ const apiKey=process.env.PRIVATE_KEYS ? process.env.PRIVATE_KEYS.split(',') : []
 
 function verifyApplication(privateKey) {
     if(!apiKey.includes(privateKey)){
-      return apiKey;
+      return true;
     }
     return false;
   }
